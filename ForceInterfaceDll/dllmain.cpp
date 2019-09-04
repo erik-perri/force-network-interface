2d3cfe7f97224e18e0093f2b0e767a9e3b44b715
Erik Perri <erik.perri@gmail.com>
2019-09-04 00:39:06 -0400

    Added test hook

--- ForceInterfaceDll/dllmain.cpp
+++ ForceInterfaceDll/dllmain.cpp
@@ -1,19 +1,50 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <WinSock2.h>
+#include <detours.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "detours.lib")
+
+int (WINAPI* Real_send)(SOCKET s, CONST char* buf, int len, int flags) = send;
+
+int WINAPI Hooked_send(SOCKET s, char* buf, int len, int flags)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	OutputDebugString(L"ForceNetworkInterface: ForceInterfaceDll: Hooked_send");
+
+	return Real_send(s, buf, len, flags);
 }
 
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  dwReasonForCall, LPVOID lpReserved)
+{
+	switch (dwReasonForCall)
+	{
+	case DLL_PROCESS_ATTACH:
+		OutputDebugString(L"ForceNetworkInterface: ForceInterfaceDll: DLL_PROCESS_ATTACH");
+
+		DetourRestoreAfterWith();
+
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+
+		DetourAttach(&(PVOID&)Real_send, Hooked_send);
+
+		DetourTransactionCommit();
+		break;
+	case DLL_PROCESS_DETACH:
+		OutputDebugString(L"ForceNetworkInterface: ForceInterfaceDll: DLL_PROCESS_DETACH");
+
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+
+		DetourDetach(&(PVOID&)Real_send, Hooked_send);
+
+		DetourTransactionCommit();
+		break;
+	}
+	return TRUE;
+}
+
+// Detours requires an export in the DLL to inject properly
+void __declspec(dllexport) Unused()
+{
+}
