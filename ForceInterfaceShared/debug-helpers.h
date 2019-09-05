94ab50256216809e1d9e645ba9b2c7403e4b8aee
Erik Perri <erik.perri@gmail.com>
2019-09-04 22:24:13 -0400

    Cleanup

--- ForceInterfaceShared/debug-helpers.h
+++ ForceInterfaceShared/debug-helpers.h
@@ -4,7 +4,7 @@ int __cdecl OutputDebugLineA(LPCSTR format, ...);
 int __cdecl OutputDebugLineW(LPCWSTR format, ...);
 
 #ifdef UNICODE
-#define OutputDebugLine  OutputDebugLineW
+#define OutputDebugLine OutputDebugLineW
 #else
-#define OutputDebugLine  OutputDebugLineA
+#define OutputDebugLine OutputDebugLineA
 #endif // !UNICODE
