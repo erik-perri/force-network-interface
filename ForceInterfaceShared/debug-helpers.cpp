570b2ca1840f2d0961868f97a8133c832666b702
Erik Perri <erik.perri@gmail.com>
2019-09-14 23:10:48 -0400

    Improved debug functions

--- ForceInterfaceShared/debug-helpers.cpp
+++ ForceInterfaceShared/debug-helpers.cpp
@@ -1,31 +1,58 @@
 #include "pch.h"
-#include "framework.h"
 #include "debug-helpers.h"
 
-int __cdecl OutputDebugLineA(LPCSTR format, ...)
+int __cdecl OutputDebugLineA(LPCSTR lpszFormat, ...)
 {
-    CHAR szLine[2048];
+    va_list argList;
+    va_start(argList, lpszFormat);
 
-    va_list argptr;
-    va_start(argptr, format);
-    int ret = _vsnprintf_s(szLine, sizeof(szLine), format, argptr);
-    va_end(argptr);
+    size_t nRequiredChars = _vscprintf(lpszFormat, argList);
+    if (nRequiredChars == -1) {
+        va_end(argList);
+        return -1;
+    }
 
-    OutputDebugStringA(szLine);
+    nRequiredChars += (size_t)1;  // Add 1 for the terminating null character
 
-    return ret;
+    std::unique_ptr<char[]> pBuffer(new char[nRequiredChars]);
+    int nCharsAdded = _vsnprintf_s(pBuffer.get(), nRequiredChars, _TRUNCATE, lpszFormat, argList);
+
+    va_end(argList);
+
+    if (nCharsAdded != -1) {
+        std::string strLine(pBuffer.get(), pBuffer.get() + nCharsAdded);
+        strLine.append("\r\n");
+
+        OutputDebugStringA(strLine.c_str());
+    }
+
+    return nCharsAdded;
 }
 
-int __cdecl OutputDebugLineW(LPCWSTR format, ...)
+int __cdecl OutputDebugLineW(LPCWSTR lpszFormat, ...)
 {
-    WCHAR szLine[2048];
+    va_list argList;
+    va_start(argList, lpszFormat);
 
-    va_list argptr;
-    va_start(argptr, format);
-    int ret = _vsnwprintf_s(szLine, sizeof(szLine), format, argptr);
-    va_end(argptr);
+    size_t nRequiredChars = _vscwprintf(lpszFormat, argList);
+    if (nRequiredChars == -1) {
+        va_end(argList);
+        return -1;
+    }
 
-    OutputDebugStringW(szLine);
+    nRequiredChars += (size_t)1;  // Add 1 for the terminating null character
 
-    return ret;
+    std::unique_ptr<wchar_t[]> pBuffer(new wchar_t[nRequiredChars]);
+    int nCharsAdded = _vsnwprintf_s(pBuffer.get(), nRequiredChars, _TRUNCATE, lpszFormat, argList);
+
+    va_end(argList);
+
+    if (nCharsAdded != -1) {
+        std::wstring strLine(pBuffer.get(), pBuffer.get() + nCharsAdded);
+        strLine.append(L"\r\n");
+
+        OutputDebugStringW(strLine.c_str());
+    }
+
+    return nCharsAdded;
 }
